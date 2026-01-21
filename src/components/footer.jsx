f1aedf1218466e2def476222436e0ad7894252ba
adrija-03 <adrijamitra01@gmail.com>
2026-01-22 00:47:42 +0530

    First page designed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,48 +2,58 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-[rgb(240,240,245)] mt-28'>
-            <div className='flex justify-center relative top-16'>
-            <div className='w-52'>
-                <div>
-                    <img src='https://media-assets.swiggy.com/portal/testing/seo-home/Group.svg' alt='Swiggy'></img>
+        <div className='bg-[rgb(240,240,245)] mt-28 h-screen'>
+            <div className='flex justify-center relative top-16 gap-5'>
+                <div className='w-52'>
+                    <div>
+                        <img src='https://media-assets.swiggy.com/portal/testing/seo-home/Group.svg' alt='Swiggy'></img>
+                    </div>
+                    <div className='font-medium text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>© 2025 Swiggy Limited</div>
                 </div>
-                <div className='font-medium text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>© 2025 Swiggy Limited</div>
+                <div className='w-32 flex flex-col gap-2.5'>
+                    <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Company</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>About Us</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Corporate</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Careers</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Team</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy One</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Instamart</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Dineout</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Minis</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Pyng</div>
+                </div>
+                <div className='w-32 flex flex-col gap-2.5'>
+                    <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Contact Us</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Help & Support</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Partner With Us</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Ride With Us</div>
+                </div>
+                <div className='w-32 flex flex-col gap-2.5'>
+                    <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Available in:</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Bangalore</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Gurgaon</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Hyderabad</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Delhi</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Mumbai</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Pune</div>
+                </div>
+                <div className='w-32 flex flex-col gap-2.5'>
+                    <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Life at Swiggy</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Explore With Swiggy</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy News</div>
+                    <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Snackables</div>
+                </div>
             </div>
-            <div className='w-32'>
-                <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Company</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>About Us</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Corporate</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Careers</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Team</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy One</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Instamart</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy Dineout</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Minis</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Pyng</div>
+            <div className='h-px w-[90%] mx-auto bg-[rgba(2,6,12,0.45)] mt-28'></div>
+            <div className='flex justify-center items-center pt-[30px] gap-8'>
+                <div className='font-bold text-[24px] leading-[26px] tracking-[-0.6px] text-[rgba(2,6,12,0.75)]'>For better experience, download the Swiggy app now</div>
+                <div>
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-AppStore_lg30tv" alt="" />
+                </div>
+                <div>
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-GooglePlay_1_zixjxl" alt="" />
+                </div>
             </div>
-            <div className='w-32'>
-                <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Contact Us</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Help & Support</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Partner With Us</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Ride With Us</div>
-            </div>
-            <div className='w-32'>
-                <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Available in:</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Bangalore</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Gurgaon</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Hyderabad</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Delhi</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Mumbai</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Pune</div>
-            </div>
-            <div className='w-32'>
-                <div className='font-bold text-[18px] leading-[22px] tracking-[-0.45px] text-[rgba(2,6,12,0.92)]'>Life at Swiggy</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Explore With Swiggy</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Swiggy News</div>
-                <div className='font-normal text-[16px] leading-[21px] tracking-[-0.4px] text-[rgba(2,6,12,0.6)]'>Snackables</div>
-            </div>
-        </div>
         </div>
     )
 }
--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -8,12 +8,17 @@ export default function HeroSection() {
                 <div className='w-[160px] h-[48px]'>
                     <img src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png' alt='Swiggy'></img></div>
                 <div>
-                    <ul className='flex items-center justify-around w-[130%] text-white font-[Inter] text-[16px] font-semibold leading-[21px] tracking-[-0.4px] cursor-pointer z-[10000]'>
-                        <li>Swiggy Corporate</li>
-                        <li>Partner with us</li>
-                        <li>Get the app</li>
-                        <li>Sign up</li>
-                    </ul>
+                    <div className='flex items-center justify-around w-[130%] text-white font-[Inter] text-[16px] font-semibold leading-[21px] tracking-[-0.4px] cursor-pointer z-[10000]'>
+                        <div>Swiggy Corporate</div>
+                        <div>Partner with us</div>
+                        <div class="border border-solid p-[15px] rounded-[12px] flex gap-1">
+                            <div>Get the app</div>
+                            <div className="-rotate-45">
+                                <svg width="21" height="21" viewBox="0 0 21 21" fill="none" aria-label="rating-up-down-icon" aria-hidden="false" strokecolor="#FFFFFF" fillcolor="#FFFFFF"><path d="M12.634 3.45a1 1 0 0 0-1.365 1.462l4.827 4.506c.238.221.44.41.614.577H2.996a1 1 0 0 0 0 2h13.777c-.186.18-.41.39-.677.64l-4.769 4.45a1 1 0 0 0 1.365 1.462l4.817-4.495c.546-.51 1.03-.96 1.367-1.376.365-.449.664-.979.664-1.65 0-.672-.299-1.201-.664-1.65-.338-.415-.821-.866-1.367-1.376z" fill="#FFFFFF" fill-opacity="0.92"></path></svg>
+                            </div>
+                        </div>
+                        <div class="bg-black rounded-[16px] p-[15px]">Sign up</div>
+                    </div>
                 </div>
             </div>
             <div className='flex items-center justify-center flex-col pt-16 pb-10 px-0 relative top-2'>
