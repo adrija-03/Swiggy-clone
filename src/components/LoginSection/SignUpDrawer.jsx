8827eb7e92fee90e25fa79ac95229d1d99a2a785
adrija-03 <adrijamitra01@gmail.com>
2026-08-16 21:04:15 +0530

    Bugs resolved

--- src/components/LoginSection/SignUpDrawer.jsx
+++ src/components/LoginSection/SignUpDrawer.jsx
@@ -114,7 +114,7 @@ function SignUpDrawer({ closeDrawer, onSwitchToLogin }) {
                 className="peer w-full px-4 pt-6 pb-2 text-base text-gray-900 bg-transparent outline-none font-medium"
               />
               <label
-                htmlFor="code"
+                htmlFor="referralCode"
                 className="absolute left-4 top-4 text-gray-400 text-base transition-all duration-200 pointer-events-none peer-focus:top-2 peer-focus:text-xs peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:text-gray-500"
               >
                 Referral code
@@ -122,15 +122,15 @@ function SignUpDrawer({ closeDrawer, onSwitchToLogin }) {
             </div>}
 
 
-            <div 
-            onClick={() => setReferral(prev => !prev)}
-            className='text-[#5d8ed5] cursor-pointer'>Have a referral code?</div>
+            <button
+              onClick={() => setReferral(prev => !prev)}
+              className='text-[#5d8ed5] cursor-pointer'>Have a referral code?</button>
 
             <button
               type="submit"
               className="mt-4 w-full bg-[#ff5200] text-white font-bold py-3.5 px-4 shadow-md hover:bg-[#e26e10] hover:shadow-lg transition-all uppercase tracking-wide text-sm"
             >
-              LOGIN
+              Continue
             </button>
           </form>
 
